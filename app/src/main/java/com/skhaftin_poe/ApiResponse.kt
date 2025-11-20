db40d96bf26954fa9fc1e60254256d748b208ee6
ST10299658 <128587703+ST10299658@users.noreply.github.com>
2025-11-20 12:29:28 +0200

    Delete app directory

--- app/src/main/java/com/skhaftin_poe/ApiResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.skhaftin_poe
-
-data class ApiResponse<T>(
-    val success: Boolean,
-    val data: T?,
-    val message: String? = null,
-    val count: Int? = null
-)
--- app/src/main/java/com/skhaftin_poe/Settings.kt
+++
@@ -1,8 +0,0 @@
-package com.skhaftin_poe
-
-data class Settings(
-    val soundEnabled: Boolean = true,
-    val vibrationEnabled: Boolean = true,
-
-    )
-
